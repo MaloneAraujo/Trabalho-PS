fbd8729bb1efbb86269f050c09bdff9720247c85
Malone Araujo <mmdaraujo@inf.ufpel.edu.br>
2026-09-19 15:47:57 -0300

    Create Descricao.c

---
+++ Descricao.c
@@ -0,0 +1,19 @@
+Projeto de um simulador de sistema computacional hipotético
+Parte I
+Projeto de um simulador SIC/XE, computador hipotético baseado na
+arquitetura do livro :Sytems Software: An Introduction to System
+Programming; Leland L. Beck
+Introdução
+O trabalho descrito a seguir consiste em implementar um simulador para um Computador
+Hipotético (SIC/XE), conforme apresentado no livro Sytems Software: An Introduction to
+System Programming de Leland L. Beck, com alterações e complementos de algumas funções.
+Tal sistema será composto de dois módulos que deverão operar de forma integrada: o executor
+(simulador propriamente dito) e uma interface visual.
+O resultado do trabalho deverá ser entregue com toda a documentação (programas fontes, programa
+executável, documentação formal sucinta das estruturas de dados definidas, das funções
+desenvolvidas e estratégias adotadas) pelo Github e
+A avaliação do trabalho será realizada com base nos seguintes aspectos:
+- correção do programa,
+- adequação das definições adotadas,
+- uso das técnicas básicas de programação,
+- autenticidade e domínio sobre o produto gerado,
